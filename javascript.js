a9d9a2fe57b630fedb3a6bedec695d9d33297b13
Brodie Oldfield <60982221+broldfield@users.noreply.github.com>
2023-08-02 23:48:30 +1000

    Displays real-time updates of the board on-screen Clicking input added

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
+const playBtn = document.getElementById("playGame");
+
 const gameBoard = (() => {
   let board = [
-    ["Y", "Y", "X"],
-    ["Y", "X", "X"],
-    ["X", "Y", "X"],
+    ["Z", "Z", "Z"],
+    ["Z", "Z", "Z"],
+    ["Z", "Z", "Z"],
   ];
+
+  const getBoard = () => {
+    return board;
+  };
 
   const reset = () => {
     board = [
@@ -57,12 +63,101 @@ const gameBoard = (() => {
     return 0;
   };
 
-  return { add, checkWin, reset };
+  return { add, checkWin, reset, getBoard };
 })();
+
+const playerFactory = (player) => {
+  let wins = 0;
+
+  let currentPlayer = false;
+
+  let playerType = player;
+
+  const addWins = () => {
+    wins++;
+  };
+
+  const getPlayerType = () => {
+    return playerType;
+  };
+
+  const getWins = () => {
+    return wins;
+  };
+
+  const reset = () => {
+    wins = 0;
+  };
+
+  return { getWins, getPlayerType, addWins, reset, currentPlayer };
+};
+
+const playerX = playerFactory("X");
+const playerO = playerFactory("O");
+playerX.currentPlayer = true;
 
 // gameBoard.add("X", 0, 2);
 
-console.log(`X: ${gameBoard.checkWin("X")}`);
-console.log(`Y: ${gameBoard.checkWin("Y")}`);
+const displayController = (() => {
+  const rows = [];
+  rows[0] = document.getElementById("row1").getElementsByTagName("div");
+  rows[1] = document.getElementById("row2").getElementsByTagName("div");
+  rows[2] = document.getElementById("row3").getElementsByTagName("div");
 
-gameBoard.reset();
+  const updateBoard = () => {
+    let board = gameBoard.getBoard();
+
+    for (let i = 0; i < rows.length; i++) {
+      for (let j = 0; j < rows[i].length; j++) {
+        board[i][j] == "Z"
+          ? (rows[i][j].textContent = "")
+          : (rows[i][j].textContent = board[i][j]);
+        // rows[i][j].textContent = board[i][j];
+      }
+    }
+  };
+
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    for (let colIndex = 0; colIndex < rows[rowIndex].length; colIndex++) {
+      rows[rowIndex][colIndex].addEventListener(
+        "click",
+        (e) => {
+          if (playerX.currentPlayer) {
+            gameBoard.add(playerX.getPlayerType(), rowIndex, colIndex);
+            // console.log(rowIndex, colIndex, playerX.getPlayerType());
+            // console.log("X");
+            console.log(gameBoard.getBoard());
+          }
+          if (playerO.currentPlayer) {
+            gameBoard.add(playerO.getPlayerType(), rowIndex, colIndex);
+            console.log("O");
+          }
+          e.stopPropagation();
+          updateBoard();
+        },
+        false
+      );
+    }
+  }
+  updateBoard();
+
+  return { updateBoard };
+})();
+
+const playGame = () => {
+  gameBoard.reset();
+  displayController.updateBoard();
+};
+
+playBtn.addEventListener("click", () => {
+  playGame();
+  console.log("Clicked");
+}),
+  false;
+
+// console.log(gameBoard.getBoard());
+
+// console.log(`X: ${gameBoard.checkWin("X")}`);
+// console.log(`Y: ${gameBoard.checkWin("Y")}`);
+
+// gameBoard.reset();
